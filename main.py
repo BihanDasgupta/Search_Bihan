fed401c963104bdcf2ad2f6cf8ab27c01cf7c3c4
Bihan Dasgupta <87448726+BihanDasgupta@users.noreply.github.com>
2024-10-13 14:04:03 -0700

    change gpt model to 3.5-turbo (too broke for gpt4)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def retrieve_info(query):
 
 
 # Call to OpenAI LLM, gpt-3.5-turbo-1106 (change model as needed)
-llm = ChatOpenAI(temperature=0, model="gpt-4")
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
 
 # Prompt template feeding question (user's query) and relavant data (corpus)
 
